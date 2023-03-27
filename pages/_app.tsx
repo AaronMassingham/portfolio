7f0c3428dbfea94aebaea4ca2deee807e874580a
AaronM <aaron.massingham@outlook.com>
2023-03-27 20:25:47 +0100

    Remove FontAwesome for lighter build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,5 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { config } from "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/fontawesome-svg-core/styles.css";
-config.autoAddCss = false;
 
 //Fonts
 import { contentFont } from "@utils/Fonts";
