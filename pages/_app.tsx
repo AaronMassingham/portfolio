5d88c44459ed78186a2321c118adca851651753b
AaronM <aaron.massingham@outlook.com>
2023-04-05 14:47:21 +0100

    Optimise fonts, update json, device comp radius

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,14 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 //Fonts
-import { contentFont } from "@utils/Fonts";
+import { ContentFont } from "@utils/Fonts";
 
 //Components
 import Layout from "@components/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<div className={contentFont.className}>
+		<div className={ContentFont.className}>
 			<Layout>
 				<Component {...pageProps} />
 			</Layout>
